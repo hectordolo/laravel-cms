ee157f46a5585cb29b1f6a3edcb7804087bc6695
Hector Dolo <hectordolo@gmail.com>
2017-01-29 21:40:49 +0800

    Editted Laratrust

---
+++ database/seeds/PermisionsTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PermisionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
